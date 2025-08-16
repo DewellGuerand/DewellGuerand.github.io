eb836c2620a92b654739bbf2a493647bb399a5bc
Guerand <guerand12004@gmail.com>
2025-08-16 21:14:12 +0200

    okay

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://GuerandDewell.github.io',
+  site: 'https://SteedStone.github.io',
   trailingSlash: 'ignore',
   vite: {
     plugins: [tailwindcss()],
